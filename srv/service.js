bee3c7bdd1a8b056738f0b8c5fe57a33a3553b91
Sebastian Goldberg <sebas.goldberg@gmail.com>
2020-07-16 18:00:09 +0000

    - Eliminação das entidades do serviço externo da API OData. - Adição de teste para obter as informações do S/4 HANA Cloud e exibir as mesmas no PDF. - Definição de timeout de 15 minutos para o serviço externo.

--- srv/service.js
+++ srv/service.js
@@ -2,10 +2,16 @@ const fs = require('fs')
 const PDFDocument = require('pdfkit');
 const { resolve } = require('path');
 
-async function getDiarioPDFReadableStream(ID){
+async function getDiarioPDFReadableStream(req, ID){
+
+    const YY1_BALANCE_A2P_API_CDS = await cds.connect.to('YY1_BALANCE_A2P_API_CDS')
+
+    const tx = YY1_BALANCE_A2P_API_CDS.transaction(req)
+
+    const results = await tx.get(`/YY1_Balance_A2P_API(P_FromPostingDate=datetime'2020-05-01T01:00:00',P_ToPostingDate=datetime'2020-05-31T01:00:00')/Results?$top=10&$skip=200&$filter=Ledger eq '0L'&$format=json`)
+
     const filename = `${__dirname}/../media/pdfkit.pdf`
     const image_filename = `${__dirname}/../media/image.png`
-    console.log(filename, image_filename);
     
     const writer = fs.createWriteStream(filename)
 
@@ -54,6 +60,11 @@ async function getDiarioPDFReadableStream(ID){
     .underline(100, 100, 160, 27, {color: "#0000FF"})
     .link(100, 100, 160, 27, 'http://google.com/');
 
+    doc.addPage()
+    .fillColor("black")
+    .fontSize(10)
+    .text(JSON.stringify(results,null,1))
+
     // Finalize PDF file
     doc.end();
 
@@ -79,7 +90,7 @@ module.exports = cds.service.impl(async function () {
             const { ID } = req.data
             return [
                 {
-                    value: await getDiarioPDFReadableStream(ID)
+                    value: await getDiarioPDFReadableStream(req, ID)
                 }
             ]
         }
